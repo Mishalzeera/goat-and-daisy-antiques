b282276024bdac62d80b0551bdfe859368945a2d
Mishalzeera <78355230+Mishalzeera@users.noreply.github.com>
2022-01-06 10:46:18 +0100

    Roll back to previous state.

--- goat_and_daisy/settings.py
+++ goat_and_daisy/settings.py
@@ -141,5 +141,4 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, STATIC_URL),
 )

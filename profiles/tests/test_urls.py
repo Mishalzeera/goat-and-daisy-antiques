e0609919619bac935a19cd0251bb6b521fc501f3
Mishalzeera <78355230+Mishalzeera@users.noreply.github.com>
2022-01-10 12:19:35 +0100

    Add login logout function tests.

--- profiles/tests/test_urls.py
+++ profiles/tests/test_urls.py
@@ -1,9 +1,19 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve 
 from profiles.views import UserProfilePage, UserSignupPage, AdminUserManagement
+from django.contrib.auth.views import LoginView, LogoutView
 
 
 class TestUrls(SimpleTestCase):
+
+    def test_login_url_is_resolved(self):
+        url = reverse('login')
+        self.assertEquals(resolve(url).func.view_class, LoginView)
+
+
+    def test_logout_url_is_resolved(self):
+        url = reverse('logout')
+        self.assertEquals(resolve(url).func.view_class, LogoutView)
 
     def test_profiles_url_is_resolved(self):
         url = reverse('profile')

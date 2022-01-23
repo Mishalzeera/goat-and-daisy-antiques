b6abeff82a965d950a04e9b54d944d165b5f34c9
Mishalzeera <78355230+Mishalzeera@users.noreply.github.com>
2022-01-23 12:20:19 +0100

    Customise admin title and Profiles app admin display.

--- goat_and_daisy/urls.py
+++ goat_and_daisy/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings 
+from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
@@ -21,11 +21,15 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('profiles/', include('django.contrib.auth.urls')),
-    path('profiles/', include('profiles.urls')),   
+    path('profiles/', include('profiles.urls')),
     path('', include('site_layout.urls')),
     path('shop/', include('shop.urls')),
     path('workshop/', include('repairs_restorals.urls')),
-] 
+]
+
+admin.site.site_header = "Goat and Daisy"
+admin.site.index_title = "Antique Shop and Repairs Admin Panel"
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
--- profiles/admin.py
+++ profiles/admin.py
@@ -1,7 +1,33 @@
 from django.contrib import admin
 from .models import Customer, StaffMember
 
-# Register your models here.
 
-admin.site.register(Customer)
-admin.site.register(StaffMember)
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'has_repairs', 'has_orders']
+
+    def has_repairs(self, customer):
+        if customer.has_active_repairs:
+            return "Yes"
+        return ""
+
+    def has_orders(self, customer):
+        if customer.has_active_shop_orders:
+            return "Yes"
+        return ""
+
+
+@admin.register(StaffMember)
+class StaffMemberAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'has_shop_permission',
+                    'has_workshop_permission']
+
+    def has_shop_permission(self, staff_member):
+        if staff_member.shop_staff:
+            return "Yes"
+        return ""
+
+    def has_workshop_permission(self, staff_member):
+        if staff_member.workshop_staff:
+            return "Yes"
+        return ""

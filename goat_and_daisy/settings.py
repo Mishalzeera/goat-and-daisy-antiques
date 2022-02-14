43f14b98d53bfba1308ecef254830732cd19a7f2
Mishalzeera <78355230+Mishalzeera@users.noreply.github.com>
2022-02-14 17:44:13 +0100

    Move AWS code to bottom of settings.

--- goat_and_daisy/settings.py
+++ goat_and_daisy/settings.py
@@ -115,25 +115,6 @@ else:
     }
 
 
-if "USE_AWS" in os.environ:
-    AWS_S3_OBJECT_PARAMETERS = {
-	'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-	'CacheControl': 'max-age=94608000',
-	}
-    AWS_STORAGE_BUCKET_NAME = "goat-and-daisy-antiques"
-    AWS_S3_REGION_NAME = "eu-central-1"
-    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-
-
-    STATICFILES_STORAGE = 'custom.storages.StaticStorage'
-    STATICFILES_LOCATION = 'static'
-    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
-    MEDIAFILES_LOCATION = 'media'
-
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 
 # Password validation
@@ -219,3 +200,23 @@ LOCAL_STRIPE_WH_SECRET = os.environ.get('LOCAL_STRIPE_WH_SECRET', '')
 # purchase as well as how long they have til their session expires. Used
 # in Invoices app views.
 CUSTOMER_SESSION_EXPIRY = 600
+
+if "USE_AWS" in os.environ:
+    AWS_S3_OBJECT_PARAMETERS = {
+	'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+	'CacheControl': 'max-age=94608000',
+	}
+    AWS_STORAGE_BUCKET_NAME = "goat-and-daisy-antiques"
+    AWS_S3_REGION_NAME = "eu-central-1"
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+
+    STATICFILES_STORAGE = 'custom.storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'

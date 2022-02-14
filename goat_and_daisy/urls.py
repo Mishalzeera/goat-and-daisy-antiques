759d186bdc5764280e040907635b1be6676c7cdd
Mishalzeera <78355230+Mishalzeera@users.noreply.github.com>
2022-02-14 18:03:45 +0100

    Adapt AWS keys in settings.

--- goat_and_daisy/urls.py
+++ goat_and_daisy/urls.py
@@ -17,6 +17,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+import os 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,6 +32,7 @@ urlpatterns = [
 admin.site.site_header = "Goat and Daisy"
 admin.site.index_title = "Antique Shop and Repairs Admin Panel"
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
+if not "USE_AWS" in os.environ:
+    if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,
+                            document_root=settings.MEDIA_ROOT)
